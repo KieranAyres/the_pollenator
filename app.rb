ef1fb93a91c80e20f955ca838ed67185d880626e
KieranAyres <k.ayres183@outlook.com>
2024-06-17 23:09:47 +0100

    experimenting

---
+++ app.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'uri'
+require 'net/http'
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+
+response = http.request(request)
+puts response.read_body
